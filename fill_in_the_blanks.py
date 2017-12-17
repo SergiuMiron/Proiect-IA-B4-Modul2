cb48ee91db522ad4815b2c4f2d041db7f9e7a8b5
Razvan <razvan.oprea96@yahoo.com>
2017-12-17 23:11:31 +0200

    Articularea cuvintelor

--- fill_in_the_blanks.py
+++ fill_in_the_blanks.py
@@ -21,14 +21,24 @@ answer_id_file =  open(r'C:\Users\razva\Desktop\Fill_in_the_blanks\answers_vm.tx
 question_id = 0
 answer_id = 0
 
-#1st type of questions (complete the definition):
+
+# Intrebari pe baza definitiei, cautam dupa cuvintele cheie: "este", "defineste", "reprezinta".
+# Intrebari usoare, cu un singur raspuns corect, mai exact definitia
+# Merge destul de greut, raman de vazut de ce
+# First type of questions
 random.shuffle(subjects)
 for inst in subjects:
-    if inst["definitie"].startswith("Este") or inst["definitie"].startswith("este"):
+    #if inst["definitie"].startswith("Este") or inst["definitie"].startswith("este"):
+    if "este" in inst["definitie"] or "Este" in inst["definitie"]:
         answer_data = inst["nume"]
         question_data = str.lower(inst["definitie"])
-        # verific doar cuvintele ce le pot articula (fara diacritice si care exista in dex)
-        # momentan pornesc de la premiza ca definitiile cu 2-3 cuvinte in nume sunt deja articulate ("ex: Cutia craniana")
+        if inst["definitie"].startswith("Este"):
+            question_data_final = question_data
+        else:
+            index_space = question_data.index("este")
+            question_data_final = question_data[index_space:]
+        # Verific doar cuvintele ce le pot articula (fara diacritice si care exista in dex)
+        # Definitiile formate din 2 cuvinte -> il articulez doar pe primul ("margine anterioara -> marginea anterioara")
         if answer_data.count(" ") == 0:
             if ArticulateWord(str.lower(answer_data)) is not None:
                 if str(ArticulateWord(str.lower(answer_data))) != "Eroare":
@@ -37,19 +47,22 @@ for inst in subjects:
                     answer = [answer_id, question_id + 1, answer_data_final, True]
                     answer_id_file.write(str(answer) + '\n')
         else:
-            answer_data_final = answer_data
-            answer_id = answer_id + 1
-            answer = [answer_id, question_id + 1, answer_data_final, True]
-            answer_id_file.write(str(answer) + '\n')
+            index_space = answer_data.index(" ")
+            first_word = answer_data[0:index_space]
+            if ArticulateWord(str.lower(first_word)) is not None:
+                if str(ArticulateWord(str.lower(first_word))) != "Eroare":
+                    answer_data_final = ArticulateWord(str.lower(first_word)).title() + answer_data[index_space:]
+                    answer_id = answer_id + 1
+                    answer = [answer_id, question_id + 1, answer_data_final, True]
+                    answer_id_file.write(str(answer) + '\n')
 
         # daca am putut construi un raspuns valid adaugam si intrebarea
         if question_id < answer_id:
             question_id = question_id + 1
-            question = [question_id, inst["domeniu"], "Easy", "... " + question_data, "FITB"]
+            question = [question_id, inst["domeniu"], "Easy", "... " + question_data_final, "FITB"]
             question_id_file.write(str(question) + '\n')
 
 
 question_id_file.close()
 answer_id_file.close()
-
 
