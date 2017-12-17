f0cf214b142848e04526c610a76fee44f3f2d475
Miron Sergiu <sergiuu.mihai@yahoo.com>
2017-12-17 14:47:00 +0200

    Added another type of question

--- ProiectIA.py
+++ ProiectIA.py
@@ -2,10 +2,6 @@ import json
 import random
 
 
-
-#TODO - CHECK FOR DUPLICATES IN QUESTIONS/ANSWERS - FIND A WAY TO MEASURE DIFFICULTY - MATCH BY SPLITTING THE DEFINITIONS OF A SUBJECT IN TWO
-
-
 #Change path
 with open(r'C:\Users\Sergiu\Desktop\fisier1_subiecte.txt',  'r' , encoding = "utf8") as data_file:
     json_data_subiecte = data_file.read()
@@ -28,45 +24,91 @@ answer_id = 0
 question_id_file = open(r'C:\Users\Sergiu\Desktop\questions_vm.txt','w', encoding = "utf8")
 answer_id_file =  open(r'C:\Users\Sergiu\Desktop\answers_vm.txt','w', encoding = "utf8")
 
+#First type of question
 for inst in subjects:
     if inst["noduri_legate"] != []:
-        question_id = question_id + 1
-        question = [question_id,inst["domeniu"],"Easy","Ce componenta face parte din " + inst["nume"],"Variante multiple"]
+        if inst["arc_nod"].count("contine") > 0 and inst["arc_nod"].count("contine") == len(inst["arc_nod"]):
+            question_id = question_id + 1
+            question = [question_id,inst["domeniu"],"Easy","Ce componenta face parte parte din " + inst["nume"],"Variante multiple"]
 
-        question_id_file.write(str(question))
-        question_id_file.write("\n")
-        bool = True
-        nr_raspunsuri_corecte = 1
-        nr_raspunsuri_totale = 1
-        #print(inst["noduri_legate"])
-        for id_nod in inst["noduri_legate"]:
-           # if(len(inst["noduri_legate"]) == 1)
-                 #nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                 x = [i for i in subjects if i["id"] == id_nod]
-                 y = [i for i in subjects if i["id"] != id_nod]
-                 random.shuffle(y)
-                 if nr_raspunsuri_corecte <= 3:
-                    if x != []:
-                        x = x[0]
-                        bool = True
-                        nr_raspunsuri_corecte = nr_raspunsuri_corecte + 1
-                        nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                        #print(x["nume"])
-                        #print(inst["nume"])
-                        #print(inst["id"])
-                        answer_id = answer_id + 1
-                        answer = [answer_id, question_id,x["nume"],bool ]
-                        answer_id_file.write(str(answer))
-                        answer_id_file.write("\n")
-                 if (nr_raspunsuri_totale <= 5):
-                     if y != []:
-                         y = y[0]
-                         bool = False
-                         nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                         answer_id = answer_id + 1
-                         answer = [answer_id,question_id,y["nume"],bool]
-                         answer_id_file.write(str(answer))
-                         answer_id_file.write("\n")
+            question_id_file.write(str(question))
+            question_id_file.write("\n")
+            bool = True
+            nr_raspunsuri_corecte = 1
+            nr_raspunsuri_totale = 1
+            #print(inst["noduri_legate"])
+            for id_nod in inst["noduri_legate"]:
+               # if(len(inst["noduri_legate"]) == 1)
+                     #nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                     x = [i for i in subjects if i["id"] == id_nod]
+                     y = [i for i in subjects if i["id"] != id_nod]
+                     random.shuffle(y)
+                     if nr_raspunsuri_corecte <= 3:
+                        if x != []:
+                            x = x[0]
+                           # if ( x["arc_nod"] == ["contine"]):
+                            bool = True
+                            nr_raspunsuri_corecte = nr_raspunsuri_corecte + 1
+                            nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                            #print(x["nume"])
+                            #print(inst["nume"])
+                            #print(inst["id"])
+                            answer_id = answer_id + 1
+                            answer = [answer_id, question_id,x["nume"],bool ]
+                            answer_id_file.write(str(answer))
+                            answer_id_file.write("\n")
+                     if (nr_raspunsuri_totale <= 5):
+                         if y != []:
+                             y = y[0]
+                             bool = False
+                             nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                             answer_id = answer_id + 1
+                             answer = [answer_id,question_id,y["nume"],bool]
+                             answer_id_file.write(str(answer))
+                             answer_id_file.write("\n")
+
+#Second type of question
+for inst in subjects:
+    if inst["noduri_legate"] != []:
+        if inst["arc_nod"].count("de_tip") > 0 and inst["arc_nod"].count("de_tip") == len(inst["arc_nod"]):
+            question_id = question_id + 1
+            question = [question_id,inst["domeniu"],"Easy","Care dintre urmatoarele variante sunt tipuri de  " + inst["nume"] + "?","Variante multiple"]
+
+            question_id_file.write(str(question))
+            question_id_file.write("\n")
+            bool = True
+            nr_raspunsuri_corecte = 1
+            nr_raspunsuri_totale = 1
+            #print(inst["noduri_legate"])
+            for id_nod in inst["noduri_legate"]:
+               # if(len(inst["noduri_legate"]) == 1)
+                     #nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                     x = [i for i in subjects if i["id"] == id_nod]
+                     y = [i for i in subjects if i["id"] != id_nod]
+                     random.shuffle(y)
+                     if nr_raspunsuri_corecte <= 3:
+                        if x != []:
+                            x = x[0]
+                           # if ( x["arc_nod"] == ["contine"]):
+                            bool = True
+                            nr_raspunsuri_corecte = nr_raspunsuri_corecte + 1
+                            nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                            #print(x["nume"])
+                            #print(inst["nume"])
+                            #print(inst["id"])
+                            answer_id = answer_id + 1
+                            answer = [answer_id, question_id,x["nume"],bool ]
+                            answer_id_file.write(str(answer))
+                            answer_id_file.write("\n")
+                     if (nr_raspunsuri_totale <= 5):
+                         if y != []:
+                             y = y[0]
+                             bool = False
+                             nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                             answer_id = answer_id + 1
+                             answer = [answer_id,question_id,y["nume"],bool]
+                             answer_id_file.write(str(answer))
+                             answer_id_file.write("\n")
 
 
 
