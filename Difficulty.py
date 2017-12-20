7820a09ca9918f23029e47f7710930fd2d4a897a
Andrew Mircius <andrew_x_2005@yahoo.com>
2017-12-20 17:43:16 +0200

    Added documentation for difficulty and distance functions

--- Difficulty.py
+++ Difficulty.py
@@ -16,14 +16,15 @@ if "Difficulty" not in sys.modules:
 # Medium: 33%-66%
 # Hard: 66%-100%
 
+# Cauta indexul din json corespunzator id-ului cautat din baza de date
 def getEntryIndexById (node_id):
     for index in range(0, len(subjects)):
         if subjects[index]['id'] == node_id:
             return index
     return -1
 
-#assume the first node(entry) is the JSON is ontology's root node
 # TODO Maybe the DB id should be passed as argument?
+# Calculeaza inaltimea maxima incepand de la root, care este indexul din fisierul json!
 def calculateMaxDepth(root):
     max = 0
     for node in subjects[root]['noduri_legate']:
@@ -32,22 +33,30 @@ def calculateMaxDepth(root):
             max = currentMax
 
     return max + 1
+#Example:
+#print(calculateMaxDepth(getEntryIndexById(100)))
 
-# TODO Call get Entry index before
-def calculateNodeDepth(current_node, answer_node_id, current_depth):
-    if current_node == getEntryIndexById(answer_node_id):
+# TODO Optimise callind getEntryIndex
+# Calculeaza distanta de la root_node la answer_node_id
+# !! Deoarece fisierul nostru cu subiecte are mai multe concepte care nu sunt legate intre ele (ca in ontologia finala)
+# !! Va trebui sa dati ca root_node conceptul in care vreti sa fie calculata distanta
+def calculateNodeDepth(root_node, answer_node_id, current_depth):
+    if root_node == getEntryIndexById(answer_node_id):
         return current_depth
     tmp =0
-    for node in subjects[current_node]['noduri_legate']:
+    for node in subjects[root_node]['noduri_legate']:
         tmp+=calculateNodeDepth(getEntryIndexById(node), answer_node_id, current_depth+1)
     return tmp
 
+# Exemplu apel: cautam subiectul cu id-ul 500 luand ca radacina a conceptului nodul cu id-ul 100 din fisier
+#print (calculateNodeDepth(getEntryIndexById(100),500, 0))
+
 #there should be "road" from the root to any of the nodes
 # 0 - Easy, 1 - Medium, 2 - Hard
-# !! Node_id is the id from the ontology, not the index in the subjects file !!
-def diffBasedOnNodeDist(node_id):
-   maxDepth = calculateMaxDepth(0)
-   nodeDepth = calculateNodeDepth(0, node_id, 0)
+# !! Root_id and Node_id is the id from the ontology, not the index in the subjects file !!
+def diffBasedOnNodeDist(root_id, node_id):
+   maxDepth = calculateMaxDepth(getEntryIndexById(root_id))
+   nodeDepth = calculateNodeDepth(getEntryIndexById(root_id), node_id, 0)
    print(maxDepth, nodeDepth)
    difficulty = nodeDepth * 100 / maxDepth;
    if difficulty < 33.3:
