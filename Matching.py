6013570579c52983167404c8edf22624f05ea63a
Vlad Scurtu <scurtuvlad@gmail.com>
2017-12-22 00:21:58 +0200

    Added difficulty for matching

--- Matching.py
+++ Matching.py
@@ -1,13 +1,12 @@
 import json
-
 # TODO - CHECK FOR DUPLICATES IN QUESTIONS/ANSWERS - FIND A WAY TO MEASURE DIFFICULTY - MATCH BY SPLITTING THE DEFINITIONS OF A SUBJECT IN TWO
 
 
 # Change path
-with open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\fisier1_subiecte.txt', 'r', encoding="utf8") as data_file:
+with open(r'C:\Users\Vlad\Desktop\ProjectIA\fisier1_subiecte.txt', 'r', encoding="utf-8-sig") as data_file:
     json_data_subiecte = data_file.read()
 
-with open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\fisier2_proprietati.txt', 'r', encoding="utf8") as data_file:
+with open(r'C:\Users\Vlad\Desktop\ProjectIA\fisier2_proprietati.txt', 'r', encoding="utf-8-sig") as data_file:
     json_data_proprietati = data_file.read()
 
 subjects = json.loads(json_data_subiecte)
@@ -20,18 +19,80 @@ print(type(properties))
 question_id = 0
 answer_id = 0
 
+import json
+import sys
+
+# Assume there is a global "subjects" json list
+
+# Dificultate bazata pe distanta;
+# Exemplu: daca nodul dat drept raspuns se afla in prima treime, plecand de la radacina ontologiei, va fi un raspuns usor
+# Easy: 0-33%
+# Medium: 33%-66%
+# Hard: 66%-100%
+
+# Cauta indexul din json corespunzator id-ului cautat din baza de date
+def getEntryIndexById (node_id):
+    for index in range(0, len(subjects)):
+        if subjects[index]['id'] == node_id:
+            return index
+    return -1
+
+# TODO Maybe the DB id should be passed as argument?
+# Calculeaza inaltimea maxima incepand de la root, care este indexul din fisierul json!
+def calculateMaxDepth(root):
+    max = 0
+    for node in subjects[root]['noduri_legate']:
+        currentMax = calculateMaxDepth(getEntryIndexById(node))
+        if currentMax > max:
+            max = currentMax
+
+    return max + 1
+#Example:
+#print(calculateMaxDepth(getEntryIndexById(100)))
+
+# TODO Optimise callind getEntryIndex
+# Calculeaza distanta de la root_node la answer_node_id
+# !! Deoarece fisierul nostru cu subiecte are mai multe concepte care nu sunt legate intre ele (ca in ontologia finala)
+# !! Va trebui sa dati ca root_node conceptul in care vreti sa fie calculata distanta
+def calculateNodeDepth(root_node, answer_node_id, current_depth):
+    if root_node == getEntryIndexById(answer_node_id):
+        return current_depth
+    tmp =0
+    for node in subjects[root_node]['noduri_legate']:
+        tmp+=calculateNodeDepth(getEntryIndexById(node), answer_node_id, current_depth+1)
+    return tmp
+
+# Exemplu apel: cautam subiectul cu id-ul 500 luand ca radacina a conceptului nodul cu id-ul 100 din fisier
+#print (calculateNodeDepth(getEntryIndexById(100),500, 0))
+
+#there should be "road" from the root to any of the nodes
+# 0 - Easy, 1 - Medium, 2 - Hard
+# !! Root_id and Node_id is the id from the ontology, not the index in the subjects file !!
+def diffBasedOnNodeDist(root_id, node_id):
+   maxDepth = calculateMaxDepth(getEntryIndexById(root_id))
+   nodeDepth = calculateNodeDepth(getEntryIndexById(root_id), node_id, 0)
+   print(maxDepth, nodeDepth)
+   difficulty = nodeDepth * 100 / maxDepth;
+   if difficulty < 33.3:
+       return 0
+   elif difficulty > 33.3 and difficulty < 66.6:
+       return 1
+   else: return 2
+
+
 # Create files for testing question_id / answer_id
 
-question_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\questions_synonyms.txt', 'w',
+question_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\questions_synonyms.txt', 'w',
                         encoding="utf8")
-answer_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\answers_synonyms.txt', 'w',
+answer_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\answers_synonyms.txt', 'w',
                       encoding="utf8")
 
 # Match by synonyms
 for inst in subjects:
     if inst["sinonime"] != []:
         question_id = question_id + 1
-        question = [question_id, inst["domeniu"], "Easy", inst["nume"], "SingleMatch"]
+        difficultate = diffBasedOnNodeDist(subjects[0]["id"],inst["id"])
+        question = [question_id, inst["domeniu"], difficultate, inst["nume"], "SingleMatch"]
         # print(inst["nume"])
 
         question_id_file.write(str(question))
@@ -51,9 +112,10 @@ for inst in subjects:
 question_id_file.close()
 answer_id_file.close()
 
-question_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\questions_properties.txt', 'w',
+
+question_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\questions_properties.txt', 'w',
                         encoding="utf8")
-answer_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\answers_properties.txt', 'w',
+answer_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\answers_properties.txt', 'w',
                       encoding="utf8")
 
 # Match by properties
@@ -68,7 +130,8 @@ for inst in properties:
             if x != []:
                 x = x[0]
                 question_id = question_id + 1
-                question = [question_id, x["domeniu"], "Easy", x["nume"], "SingleMatch"]
+                difficultate = diffBasedOnNodeDist(subjects[0]["id"],x["id"])
+                question = [question_id, x["domeniu"], difficultate, x["nume"], "SingleMatch"]
 
                 answer_id = answer_id + 1
                 answer = [answer_id, question_id, inst["nume"], "True"]
@@ -95,10 +158,9 @@ question_id_file.close()
 answer_id_file.close()
 
 
-
-question_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\questions_definition.txt', 'w',
+question_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\questions_definition.txt', 'w',
                         encoding="utf8")
-answer_id_file = open(r'C:\Users\alexa\Desktop\IA\Proiect-IA-B4-Modul2\Results\answers_definition.txt', 'w',
+answer_id_file = open(r'C:\Users\Vlad\Desktop\ProjectIA\answers_definition.txt', 'w',
                       encoding="utf8")
 
 
@@ -115,11 +177,11 @@ for inst in subjects:
             answer_id_file.write(str(answer)+ "\n")
         else:
             question_id += 1
-            question = [question_id, inst["domeniu"],"Easy",inst["nume"],"SingleMathch"]
+            difficultate = diffBasedOnNodeDist(subjects[0]["id"],inst["id"])
+            question = [question_id, inst["domeniu"],difficultate,inst["nume"],"SingleMathch"]
             questions_definitions.append(question)
             answer = [answer_id,question_id,inst["definitie"],1]
             answers_definitions.append(answer)
 
             answer_id_file.write(str(answer)+ "\n")
             question_id_file.write(str(question) + "\n")
-
