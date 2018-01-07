3820910b9a14232d6306120228e67a3a04e9f0cd
martynas5991 <alex_martynas@yahoo.com>
2018-01-07 10:19:21 +0200

    Updated database structure

--- Database.py
+++ Database.py
@@ -9,28 +9,48 @@ print "Connected"
 #  you execute all the queries you need
 cur = db.cursor()
 
+# delete all tables
+cur.execute("DROP TABLE Questions")
+print "Questions dropped"
+
+cur.execute("DROP TABLE Answers")
+print "Answers dropped"
+
+cur.execute("DROP TABLE Domains")
+print "Domains dropped"
+
+cur.execute("DROP TABLE DomainsHierarchy")
+print "DomainsHierarchy dropped"
+
+
 # Create Questions table query
 questions = "CREATE TABLE Questions (" \
-            + "Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
-            + "Field INT(6) UNSIGNED NOT NULL," \
-            + "DifficultyLevel INT(6) UNSIGNED NOT NULL," \
-            + "Statememnt VARCHAR(500)," \
-            + "Type VARCHAR(50)" \
+            + "id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
+            + "domain_id BIGINT(20) UNSIGNED," \
+            + "difficulty INT(11) UNSIGNED NOT NULL," \
+            + "body VARCHAR(500) NOT NULL," \
+            + "question_type INT(11)," \
+            + "expected_secs_to_answer INT(11) NOT NULL" \
             + ")"
 
 #Create Answers table query
 answers = "CREATE TABLE Answers (" \
-        + "Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
-        + "QuestionId INT(6) UNSIGNED NOT NULL," \
-        + "Statememnt VARCHAR(50)," \
-        + "IsValid TINYINT(1)" \
-        + ") "
+        + "id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
+        + "question_id INT(6) UNSIGNED NOT NULL," \
+        + "body VARCHAR(500)," \
+        + "is_correct TINYINT(1)" \
+        + ")"
 #Create Fields table query
-fields = "CREATE TABLE Fields (" \
-        + "Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
-        + "Name VARCHAR(50)" \
+domains = "CREATE TABLE Domains (" \
+        + "id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
+        + "name VARCHAR(50) NOT NULL" \
         + ")"
 
+domainsHierarchy = "CREATE TABLE DomainsHierarchy (" \
+        + "id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY," \
+        + "current BIGINT(20) UNSIGNED NOT NULL," \
+        + "subdomain BIGINT(20) UNSIGNED NOT NULL" \
+        + ")"
 
 cur.execute(questions)
 print "Questions created"
@@ -38,9 +58,11 @@ print "Questions created"
 cur.execute(answers)
 print "Answers created"
 
-cur.execute(fields)
-print "Fields created"
+cur.execute(domains)
+print "Domains created"
 
+cur.execute(domainsHierarchy)
+print "Domains Hierarchy created"
 # print all the first cell of all the rows
 # for row in cur.fetchall():
 #     print row[0]
