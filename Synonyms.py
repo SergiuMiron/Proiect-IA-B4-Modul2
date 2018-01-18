43330f677ef0a39d6b7f0a476206031cea7662d5
lpopa2003 <lpopa2003@yahoo.com>
2018-01-18 15:54:06 +0200

    Add files via upload

--- Synonyms.py
+++ Synonyms.py
@@ -1,30 +1,31 @@
 import json
 import random
 
-
-
-#Change path
-with open(r'd:\personal box\python\medicina\fisier1_subiecte.txt',  'r' , encoding = "utf-8-sig") as data_file:
+# Change path
+with open(r'd:\personal box\python\medicina\fisier1_subiecte.txt', 'r', encoding="utf-8-sig") as data_file:
     json_data_subiecte = data_file.read()
 
-with open(r'd:\personal box\python\medicina\fisier2_proprietati.txt', 'r' , encoding = "utf-8-sig") as data_file:
+with open(r'd:\personal box\python\medicina\fisier2_proprietati.txt', 'r', encoding="utf-8-sig") as data_file:
     json_data_proprietati = data_file.read()
 
 subjects = json.loads(json_data_subiecte)
 properties = json.loads(json_data_proprietati)
 
-#O sa avem o lista de dictionare ( 1 dictionar = 1 json / o instanta )
-#print(type(subjects))
-#print(type(properties))
+# O sa avem o lista de dictionare ( 1 dictionar = 1 json / o instanta )
+# print(type(subjects))
+# print(type(properties))
 
 question_id = 0
 answer_id = 0
 
-#Create files for testing question_id / answer_id
 
-question_id_file = open(r'd:\personal box\python\medicina\questions_vm.txt','w', encoding = "utf8")
-answer_id_file =  open(r'd:\personal box\python\medicina\answers_vm.txt','w', encoding = "utf8")
 
+# Create files for testing question_id / answer_id
+#
+question_id_file = open(r'd:\personal box\python\medicina\questions_vm.txt', 'w', encoding="utf8")
+answer_id_file = open(r'd:\personal box\python\medicina\answers_vm.txt', 'w', encoding="utf8")
+
+#Third type of question
 for inst in subjects:
     if len(inst["sinonime"])>0:
             question_id = question_id + 1
@@ -42,21 +43,33 @@ for inst in subjects:
                 answer_id_file.write("\n")
                 if nr_raspunsuri_totale == 4:
                     break
+            y=[]
             while (nr_raspunsuri_totale <= 3):
                 bool = False
                 x = [i for i in subjects if i["sinonime"] != inst["sinonime"] and len(i["sinonime"])>0 and inst["domeniu"]==i["domeniu"]]
                 random.shuffle(x)
                 if x!=None :
                     x= x[0]
+                    #y
                     for invalid in x["sinonime"]:
-                        nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                        answer_id = answer_id + 1
-                        answer = [answer_id, question_id, invalid, bool]
-                        answer_id_file.write(str(answer))
-                        answer_id_file.write("\n")
-                        if nr_raspunsuri_totale == 4:
-                            break
+                        if invalid in y:
+                            print("dublura in set 1: ")
+                            print("intrebarea numarul: ", question_id)
+                            for p in y:
+                                print(p)
+                        else:
+                            nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                            answer_id = answer_id + 1
+                            answer = [answer_id, question_id, invalid, bool]
+                            answer_id_file.write(str(answer))
+                            answer_id_file.write("\n")
+                            y.append(invalid)
+                            if nr_raspunsuri_totale == 4:
+                                break
+                            #for p in y:
+                            #    print(p)
 
+#print("\n\n\nsetul 2")
 
 
 for inst in subjects:
@@ -77,6 +90,7 @@ for inst in subjects:
                 answer_id_file.write("\n")
                 if nr_raspunsuri_totale == 4:
                     break
+            y=[]
             while (nr_raspunsuri_totale <= 3):
                 bool = False
                 x = [i for i in subjects if i["sinonime"] != inst["sinonime"] and len(i["sinonime"])>0 ]
@@ -84,13 +98,26 @@ for inst in subjects:
                 if x!=None :
                     x= x[0]
                     for invalid in x["sinonime"]:
-                        nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                        answer_id = answer_id + 1
-                        answer = [answer_id, question_id, invalid, bool]
-                        answer_id_file.write(str(answer))
-                        answer_id_file.write("\n")
-                        if nr_raspunsuri_totale == 4:
-                            break
+                        if invalid in y:
+                            print("dublura in set 2:\n")
+                            print("intrebarea numarul: ", question_id)
+                            for p in y:
+                                print(p)
+                        else:
+                            #x.pop(invalid,'')
+                            nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                            answer_id = answer_id + 1
+                            answer = [answer_id, question_id, invalid, bool]
+                            answer_id_file.write(str(answer))
+                            answer_id_file.write("\n")
+                            if nr_raspunsuri_totale == 4:
+                                break
+                            y.append(invalid)
+                        for p in y:
+                            print(p)
+
+#print("\n\n\nsetul 3")
+
 
 for inst in subjects:
     if len(inst["sinonime"])>0:
@@ -118,35 +145,30 @@ for inst in subjects:
                 answer_id_file.write("\n")
                 if nr_raspunsuri_totale == 4:
                     break
+            y=[]
             while (nr_raspunsuri_totale <= 3):
                 x = [i for i in subjects if i["sinonime"] != inst["sinonime"] and len(i["sinonime"])>0 ]
                 random.shuffle(x)
                 if x!=None :
                     x= x[0]
+                    #y
                     for invalid in x["sinonime"]:
-                        nr_raspunsuri_totale = nr_raspunsuri_totale + 1
-                        answer_id = answer_id + 1
-                        answer = [answer_id, question_id, invalid, bool]
-                        answer_id_file.write(str(answer))
-                        answer_id_file.write("\n")
-                        if nr_raspunsuri_totale == 4:
-                            break
+                        if invalid in y:
+                            print("dublura in set 3: \n")
+                            print("intrebarea numarul: ", question_id)
+                            for p in y:
+                                print(p)
+                        else:
+                            nr_raspunsuri_totale = nr_raspunsuri_totale + 1
+                            answer_id = answer_id + 1
+                            answer = [answer_id, question_id, invalid, bool]
+                            answer_id_file.write(str(answer))
+                            answer_id_file.write("\n")
+                            if nr_raspunsuri_totale == 4:
+                                break
+                        y.append(invalid)
+                        #for p in y:
+                            #print(p)
 
 question_id_file.close()
 answer_id_file.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
